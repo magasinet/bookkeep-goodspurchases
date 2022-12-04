b7a60b74744e7c33cda138321fbd3fbe1d3d6ef2
Ali Sherifi <ali.sherifi@live.com>
2022-12-04 23:34:54 +0100

    Check which method used

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 const btn = document.querySelector('#book');
 
+
 btn.addEventListener('click', () => {
-    console.log('mmm');
+    const kontantMetoden = document.querySelector('input[name="metod"]:checked').value;
+    console.log(kontantMetoden);
 });
+
+
+
+
+
+
+
+/* 
+Är kontantmetoden klickad
+Ändra bara ena tabellen
+
+Exempel
+100 25%
+
+
+*/
